bf9600a27a3836311df5b2b50051190a61e4cefc
apanitsch <apanitsch@gmail.com>
2021-04-26 20:05:59 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI.Ejercicio52Guia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
