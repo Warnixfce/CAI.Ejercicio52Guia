50608e54ab3576ceb4b2de6ce079e48bb32c3606
apanitsch <apanitsch@gmail.com>
2021-04-26 21:02:55 -0300

    Lectura / Escritura (sin probar).

---
+++ Persona.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace CAI.Ejercicio52Guia
+{
+    internal class Persona
+    {
+        private Persona(int legajo, string nombre)
+        {
+            Legajo = legajo;
+            Nombre = nombre;
+        }
+
+        public int Legajo { get; }
+        public string Nombre { get; }
+
+        internal static Persona Ingresar()
+        {
+            int legajo = 0;
+            bool ok = false;
+            Console.WriteLine("Nueva persona");
+            while (!ok)
+            {
+                Console.WriteLine("Ingrese el número de legajo.");
+                var ingreso = Console.ReadLine();
+                if (!int.TryParse(ingreso, out legajo))
+                {
+                    Console.WriteLine("Debe ingresar un numero.");
+                    continue;
+                }
+
+                if (legajo < 1)
+                {
+                    Console.WriteLine("Ingrese un numero positivo.");
+                    continue;
+                }
+
+                ok = true;
+            }
+
+            string nombre = null;
+            ok = false;
+            while (!ok)
+            {
+                Console.WriteLine("Ingrese el nombre");
+                nombre = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(nombre))
+                {
+                    Console.WriteLine("Ingrese un nombre.");
+                    continue;
+                }
+
+                if (!nombre.All(c => Char.IsLetter(c) || Char.IsWhiteSpace(c)))
+                {
+                    Console.WriteLine("Ingrese sólo letras.");
+                    continue;
+                }
+
+                nombre = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(nombre.ToLower());
+                ok = true;
+            }
+
+            return new Persona(legajo, nombre);
+        }
+
+        internal static Persona Parse(string linea)
+        {
+            var datos = linea.Split('|');
+            return new Persona(legajo: int.Parse(datos[0]), nombre: datos[1]);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,125 @@ namespace CAI.Ejercicio52Guia
 {
     class Program
     {
+        static Dictionary<int, Persona> personas = new Dictionary<int, Persona>();
+
         static void Main(string[] args)
         {
+            Console.WriteLine("a. Alta");
+            Console.WriteLine("b. Baja");
+            Console.WriteLine("g. Grabar archivo");
+            Console.WriteLine("l. Leer de archivo");
+            Console.WriteLine("s. Terminar");
+
+            while (true)
+            {
+                var tecla = Console.ReadKey();
+                if (tecla.Key == ConsoleKey.A)
+                {
+                    Alta();
+                }
+                else if (tecla.Key == ConsoleKey.B)
+                {
+                    Baja();
+                }
+                else if (tecla.Key == ConsoleKey.G)
+                {
+                    Grabar();
+                }
+                else if (tecla.Key == ConsoleKey.L)
+                {
+                    Leer();
+                }
+                else if (tecla.Key == ConsoleKey.S)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("No es una opción de la lista.");
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+
+        private static void Leer()
+        {
+            string ruta;
+            do
+            {
+                Console.WriteLine("Ingrese el nombre del archivo a leer.");
+                ruta = Console.ReadLine();
+                if (!File.Exists(ruta))
+                {
+                    Console.WriteLine("El archivo no existe");
+                }
+            }
+            while (!File.Exists(ruta));
+
+            using (var archivo = File.OpenRead(ruta))
+            {
+                using (var reader = new StreamReader(archivo))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var linea = reader.ReadLine();
+                        var persona = Persona.Parse(linea);
+                        personas.Add(persona.Legajo, persona);
+                    }
+                }
+            }
+        }
+
+        private static void Grabar()
+        {
+            Console.WriteLine("Ingrese el nombre del archivo a grabar.");
+            string ruta = Console.ReadLine();
+            if (File.Exists(ruta))
+            {
+                Console.WriteLine("El archivo ya existe. Desea sobreescribirlo?");
+                if (Console.ReadKey().Key == ConsoleKey.S)
+                {
+                    File.Delete(ruta);
+                }
+            }
+
+            using (var archivoStream = File.OpenWrite(ruta))
+            {
+                using (var writer = new StreamWriter(archivoStream))
+                {
+                    foreach (var persona in personas)
+                    {
+                        writer.WriteLine($"{persona.Value.Legajo}|{persona.Value.Nombre}");
+                    }
+                }
+            }
+        }
+
+        private static void Baja()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Alta()
+        {
+            bool seguir = true;
+            while (seguir)
+            {
+                var persona = Persona.Ingresar();
+                if (personas.ContainsKey(persona.Legajo))
+                {
+                    Console.WriteLine("El legajo indicado ya existe.");
+                }
+                else
+                {
+                    personas.Add(persona.Legajo, persona);
+                    Console.WriteLine("Desea ingresar otro [S/N]?");
+                    var tecla = Console.ReadKey();
+                    seguir = tecla.Key == ConsoleKey.S;
+                }
+            }
         }
     }
 }
